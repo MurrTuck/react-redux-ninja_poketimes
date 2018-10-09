b7db8bd6d263a7474b59a858427a1288973ab090
MurrTuck <murrtuck@gmail.com>
2018-10-09 16:06:49 -0600

    Hooking up the Navbar

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa incidunt
+        facilis vero eum est ipsa rem aperiam atque at, commodi nemo nulla
+        vitae, tenetur voluptatum corporis dignissimos exercitationem odit
+        voluptatem! Consequuntur, unde ipsam ipsum fugiat optio ad tempora
+        architecto laboriosam modi cupiditate soluta dignissimos et qui quisquam
+        tenetur quibusdam consectetur, quae ea accusamus excepturi quaerat
+        sapiente. Sit mollitia possimus eius! Fugit obcaecati, sit voluptas
+        magni ratione hic omnis quidem ab reiciendis, architecto perferendis ea
+        asperiores totam eum voluptate, facilis neque sequi aut! Necessitatibus
+        natus molestias fugit dolorum dignissimos animi pariatur! Perspiciatis
+        quisquam dicta suscipit repellat doloremque odio, hic obcaecati est
+        debitis quae iure quaerat pariatur esse sapiente dolore enim sequi vero
+        velit dolorem, vel quam. Fuga alias eveniet animi officiis. Optio
+        doloremque aut, suscipit ullam omnis ipsa. Doloribus, numquam reiciendis
+        assumenda hic dolores provident iste, asperiores voluptatum deleniti
+        labore autem perspiciatis blanditiis unde consectetur obcaecati. Sunt
+        deleniti beatae eius incidunt!
+      </p>
+    </div>
+  );
+};
+
+export default About;

2672fa81130349ae5f69845d66f7e0d93b51519e
MurrTuck <murrtuck@gmail.com>
2018-10-10 21:42:21 -0600

    Using axios and pulling from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa incidunt
-        facilis vero eum est ipsa rem aperiam atque at, commodi nemo nulla
-        vitae, tenetur voluptatum corporis dignissimos exercitationem odit
-        voluptatem! Consequuntur, unde ipsam ipsum fugiat optio ad tempora
-        architecto laboriosam modi cupiditate soluta dignissimos et qui quisquam
-        tenetur quibusdam consectetur, quae ea accusamus excepturi quaerat
-        sapiente. Sit mollitia possimus eius! Fugit obcaecati, sit voluptas
-        magni ratione hic omnis quidem ab reiciendis, architecto perferendis ea
-        asperiores totam eum voluptate, facilis neque sequi aut! Necessitatibus
-        natus molestias fugit dolorum dignissimos animi pariatur! Perspiciatis
-        quisquam dicta suscipit repellat doloremque odio, hic obcaecati est
-        debitis quae iure quaerat pariatur esse sapiente dolore enim sequi vero
-        velit dolorem, vel quam. Fuga alias eveniet animi officiis. Optio
-        doloremque aut, suscipit ullam omnis ipsa. Doloribus, numquam reiciendis
-        assumenda hic dolores provident iste, asperiores voluptatum deleniti
-        labore autem perspiciatis blanditiis unde consectetur obcaecati. Sunt
-        deleniti beatae eius incidunt!
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
